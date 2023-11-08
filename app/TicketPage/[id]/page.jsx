f13f9208165c3d2e131606e946b790e06b08fac6
aries grey <ariesgrey88@gmail.com>
2023-11-08 00:14:19 -0600

    completed tutorial, CRUD functionality established

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,7 +1,30 @@
-const TicketPage = ({params}) => {
-  return (
-    <div>TicketPage {params.id}</div>
-  )
-}
+const { default: TicketForm } = require("@/app/(components)/TicketForm");
 
-export default TicketPage
+const getTicketById = async (id) => {
+	const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+		cache: "no-store",
+	});
+
+	if (!res.ok) {
+		throw new Error("Failed to get Ticket.");
+	}
+
+	return res.json();
+};
+
+const TicketPage = async ({ params }) => {
+	const EDITMODE = params.id === "new" ? false : true;
+	let updateTicketData = {};
+
+	if (EDITMODE) {
+		updateTicketData = await getTicketById(params.id);
+		updateTicketData = updateTicketData.foundTicket;
+	} else {
+		updateTicketData = {
+			_id: "new",
+		};
+	}
+	return <TicketForm ticket={updateTicketData} />;
+};
+
+export default TicketPage;
